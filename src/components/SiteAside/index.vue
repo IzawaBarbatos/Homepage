2203f8f9101045e5b786b6688c9fa51da64c2a3d
icore <izawa3110@gmail.com>
2022-04-15 14:19:17 +0800

    add project code

---
+++ src/components/SiteAside/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="site-aside-container">
+    <Avatar url="../../../public/image/head.jpg" />
+    <h1 class="title">icore的小窝</h1>
+    <Menu />
+    <Contact />
+    <p class="footer">
+      粤ICP备17001816号
+    </p>
+  </div>
+</template>
+
+<script>
+import Avatar from "@/components/Avatar";
+import Menu from "./Menu";
+import Contact from "./Contact";
+export default {
+  components: {
+    Avatar,
+    Menu,
+    Contact,
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+.site-aside-container {
+  width: 100%;
+  height: 100%;
+  background: @dark;
+  padding: 20px 0;
+  box-sizing: border-box;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+.avatar-container {
+  margin: 0 auto;
+}
+.footer {
+  text-align: center;
+  font-size: 12px;
+}
+.title {
+  font-size: 1.2em;
+  color: #fff;
+  text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './App.vue';
+import "./styles/global.less";
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
